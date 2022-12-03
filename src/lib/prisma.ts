caf9e1661b53baee1428c2e15cb0c3a8030e9405
Tin Nguyen <hopthucuatin@gmail.com>
2022-12-03 18:24:09 +0700

    feat(customer): add internet banking

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -12,7 +12,7 @@ if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
 } else {
   if (!global.prisma) {
-    global.prisma = new PrismaClient({ log: ["query"] });
+    global.prisma = new PrismaClient({ log: [] });
   }
   prisma = global.prisma;
 }
