652087100c56f44d90179a1c7577d6f328a0bf04
Tin Nguyen <hopthucuatin@gmail.com>
2022-12-06 21:50:47 +0700

    fix: get all recipients

--- src/pages/api/recipients/index.ts
+++ src/pages/api/recipients/index.ts
@@ -8,11 +8,6 @@ import {
   COOKIES_TOKEN_NAME,
 } from "../../../server/database/tokenService";
 
-const getAllRecipientsSchema = z.object({
-  offset: z.number().optional(),
-  limit: z.number().optional(),
-});
-
 export default catchAsync(async function handle(req, res) {
   switch (req.method) {
     case "POST": {
@@ -49,7 +44,6 @@ export default catchAsync(async function handle(req, res) {
       } = await TokenService.requireAuth(req);
 
       const { offset, limit } = req.query;
-      validateSchema(getAllRecipientsSchema, req.query);
       const recipients = await RecipientService.getRecipientsByCustomerId(
         id,
         parseInt((offset as string) || "0"),
