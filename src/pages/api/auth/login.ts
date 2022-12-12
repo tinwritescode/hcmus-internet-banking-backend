3aee4f0cacc4bb1b869aedfef665a9f7e6a9249f
Tin Nguyen <hopthucuatin@gmail.com>
2022-12-12 18:30:59 +0700

    chore: remove auth require login

--- src/pages/api/auth/login.ts
+++ src/pages/api/auth/login.ts
@@ -1,10 +1,8 @@
 import { ApiError } from "./../../../base/baseResponse";
 import { GoogleRecaptchaService } from "./../../../server/googleRecaptchaService";
-import { TokenService } from "./../../../server/database/tokenService";
 import { z } from "zod";
 import { catchAsync, validateSchema } from "../../../base/catchAsync";
 import { CustomerService } from "../../../server/database/customerService";
-import { NextApiRequest } from "next";
 
 const loginValidate = z.object({
   email: z.string().email(),
@@ -16,7 +14,6 @@ export default catchAsync(async function handle(req, res) {
   switch (req.method) {
     case "POST":
       validateSchema(loginValidate, req.body);
-      await TokenService.requireNotAuth(req);
 
       // eslint-disable-next-line no-case-declarations
       const { email, password, captchaValue } = req.body;
