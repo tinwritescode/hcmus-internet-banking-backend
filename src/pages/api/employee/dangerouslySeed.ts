6fbd699f0a80571725a945a792ec4198d86df5ea
Tin Nguyen <hopthucuatin@gmail.com>
2022-12-18 20:29:02 +0700

    feat: add seed test

---
+++ src/pages/api/employee/dangerouslySeed.ts
@@ -0,0 +1,40 @@
+import { NextApiResponse } from "next";
+import { NextApiRequest } from "next";
+import { EmployeeService } from "./../../../lib/database/employeeService";
+
+const employeesData = [
+  {
+    email: "employee1@yopmail.com",
+    firstName: "Wednesday",
+    lastName: "Addams",
+    password: "password",
+    employeeType: "ADMIN",
+  },
+  {
+    email: "employee2@yopmail.com",
+    firstName: "Thurday",
+    lastName: "Addams",
+    password: "password",
+    employeeType: "EMPLOYEE",
+  },
+];
+
+export async function handle(req: NextApiRequest, res: NextApiResponse) {
+  const env = process.env.NODE_ENV;
+
+  if (env !== "development") {
+    res.status(403).json({ message: "Forbidden" });
+    return;
+  }
+
+  console.log(`Start seeding ...`);
+  for (const u of employeesData) {
+    // @ts-ignore
+    const user = await EmployeeService.createEmployee(u);
+    console.log(`Created user with id: ${user.id}`);
+  }
+
+  console.log(`Seeding finished.`);
+
+  res.status(200).json({ message: "Seeding finished." });
+}
