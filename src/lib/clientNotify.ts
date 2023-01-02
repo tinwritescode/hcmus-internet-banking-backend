fc999590ac3215863c07ab8b0fee9d973e890fe5
mie6996 <nguyenxuanphat060301@gmail.com>
2023-01-02 15:44:29 +0700

    fix notify url

--- src/lib/clientNotify.ts
+++ src/lib/clientNotify.ts
@@ -1,13 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
-const isDev = process.env.NODE_ENV === 'development';
-
-const urlApiNotifyService = isDev
-  ? 'http://localhost:9000'
-  : process.env.API_NOTIFY_SERVICE;
-
 const clientNotify: AxiosInstance = axios.create({
-  baseURL: urlApiNotifyService,
+  baseURL: process.env.API_NOTIFY_SERVICE,
   headers: {
     // eslint-disable-next-line @typescript-eslint/naming-convention
     'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
