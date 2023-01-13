964eb3c6f895d67cc7c354cc4173f9fbc209af9d
tuilakie <ntneik15@gmail.com>
2023-01-14 00:32:33 +0700

    update external karmabank

--- src/pages/api/interbanks/karma.ts
+++ src/pages/api/interbanks/karma.ts
@@ -1,4 +1,3 @@
-import { RecipientService } from "./../../../lib/database/recipientService";
 import { TransactionService } from "../../../lib/database/transactionService";
 import { z } from "zod";
 import { ApiError } from "../../../core/baseResponse";
@@ -6,77 +5,44 @@ import { catchAsync, validateSchema } from "../../../core/catchAsync";
 import { CustomerService } from "../../../lib/database/customerService";
 import { TokenService } from "../../../lib/database/tokenService";
 
-const internalTransferSchema = z.object({
+const externalTransferSchema = z.object({
   amount: z.preprocess(BigInt, z.bigint()).refine((amount) => amount > 0),
   to: z.string(),
   message: z.string().optional(),
   token: z.string(),
   payer: z.enum(["sender", "receiver"]),
-  saveInfo: z
-    .string()
-    .refine((value) => value === "true", { message: "Invalid value" }),
 });
 
 export default catchAsync(async function handle(req, res) {
   switch (req.method) {
     case "POST": {
-      validateSchema(internalTransferSchema, req.body);
+      validateSchema(externalTransferSchema, req.body);
       const {
         payload: { id },
       } = await TokenService.requireAuth(req);
 
-      const data = internalTransferSchema.safeParse(req.body);
+      const data = externalTransferSchema.safeParse(req.body);
 
       if (!data.success) {
         throw new ApiError("Invalid request", 400);
       }
 
-      const { amount, to, message, token, payer, saveInfo } = data.data;
+      const { amount, to, message, token, payer } = data.data;
 
       await TransactionService.verifyTransactionToken(token);
 
-      const receiverId = await CustomerService.getCustomerIdByBankNumber(to);
-
-      if (!receiverId) {
-        throw new ApiError("Invalid bank number", 400);
-      }
+      const fromCustomer = await CustomerService.getCustomerById(id, {
+        withBalance: false,
+        withEmail: false,
+      });
 
       const result = await CustomerService.transferInternally({
-        from: id,
-        to: receiverId,
+        from: fromCustomer.accountNumber,
+        to: to,
         amount,
         message,
         payer,
       });
-
-      if (saveInfo) {
-        await RecipientService.createRecipient({
-          accountNumber: to,
-          internalBankCustomer: {
-            connect: {
-              id,
-            },
-          },
-          customerRecipient: {
-            connectOrCreate: {
-              where: {
-                // eslint-disable-next-line @typescript-eslint/naming-convention
-                customerId_recipientId: {
-                  customerId: id,
-                  recipientId: to,
-                },
-              },
-              create: {
-                customerId: id,
-              },
-            },
-          },
-        }).catch(() => {
-          // ignore already exists error
-        });
-
-        console.log("result", result);
-      }
 
       res.status(200).json({ data: result });
       break;
